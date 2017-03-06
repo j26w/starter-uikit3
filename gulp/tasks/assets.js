278a5c3a27c80a17483b83b777969af59db85cdb
j26w <jw2626@gmail.com>
2017-03-07 00:49:24 +0100

    converted most of homepage markup, style and js to uikit3

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -34,9 +34,9 @@ gulp.task('scripts', () =>
   ])
     .pipe(newer('.tmp/assets/javascript/index.js', {dest: '.tmp/assets/javascript', ext: '.js'}))
     .pipe(when(!argv.prod, sourcemaps.init()))
-    .pipe(babel({
-      presets: ['es2015']
-    }))
+    //.pipe(babel({
+    //  presets: ['es2015']
+    // }))
     .pipe(concat('index.js'))
     .pipe(size({
       showFiles: true
